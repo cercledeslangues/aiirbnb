674eecdf9453b967c099fc9f1d00f6286ea03dde
francoisfourmentin <francois.fourmentin@tuteurprive.com>
2017-10-28 18:05:40 +0000

    Modification des vues devise

--- db/migrate/20171028160821_add_full_name_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddFullNameToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :Fullname, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028160821) do
+ActiveRecord::Schema.define(version: 20171028172425) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20171028160821) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "Fullname"
+    t.string   "fullname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
