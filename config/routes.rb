510c20ac8e37f4d7dd12fc852cb9d1731efec576
francoisfourmentin <francois.fourmentin@tuteurprive.com>
2017-11-07 16:10:56 +0000

    AJAX date d'arrivée

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     resources :reservations, only: [:create]
   end
   resources :photos
+  
+get '/preload' => 'reservations#preload'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
